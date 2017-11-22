c07ce6d14130c937b462d11725455dcdd3c19333
ridwann <ridwannizam@yahoo.com>
2017-11-22 21:06:47 +0300

    UI changes

---
+++ happens-now-front/src/main/java/org/xonyne/events/mbean/UserConfiguration.java
@@ -0,0 +1,34 @@
+package org.xonyne.events.mbean;
+
+import java.util.Locale;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean(name="userConfiguration")
+@SessionScoped
+public class UserConfiguration {
+
+    private Locale locale;
+
+    @PostConstruct
+    public void init() {
+        locale = FacesContext.getCurrentInstance().getExternalContext().getRequestLocale();
+    }
+
+    public Locale getLocale() {
+        return locale;
+    }
+
+    public String getLanguage() {
+        return locale.getLanguage();
+    }
+
+    public void setLanguage(String language) {
+        locale = new Locale(language);
+        FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
+    }
+
+}
--- happens-now-front/src/main/java/org/xonyne/events/mbean/UserLoginView.java
+++ happens-now-front/src/main/java/org/xonyne/events/mbean/UserLoginView.java
@@ -1,18 +1,10 @@
 package org.xonyne.events.mbean;
 
-import java.io.IOException;
-
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
-import javax.faces.event.ActionEvent;
-import javax.servlet.http.HttpSession;
 
-import org.primefaces.context.RequestContext;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
-import org.springframework.context.annotation.Scope;
-import org.springframework.web.client.RestTemplate;
 import org.xonyne.events.config.AppContext;
 import org.xonyne.events.dto.UserDto;
 import org.xonyne.events.service.EventsService;
@@ -46,12 +38,8 @@ public class UserLoginView {
    
     public String login() {
         FacesMessage message = null;
-        
-//        CloseableHttpClient httpClient = HttpClients.custom().setSSLHostnameVerifier(new NoopHostnameVerifier()).build();
-//        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
-//        requestFactory.setHttpClient(httpClient);
-
         String target = "login";
+        
         UserDto user = AppContext.getEvetnsService().login(username, password);
 		
         if(user != null ) {
@@ -63,5 +51,10 @@ public class UserLoginView {
         }
          
         return target;
-    }   
+    }
+    
+    public String logout(){
+    	FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+    	return "login";
+    }
 }
